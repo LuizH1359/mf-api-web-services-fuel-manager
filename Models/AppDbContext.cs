99ee33bcbb4a0cabb71d08667b373ff19a4e64d8
Luiz <luyzrocha99@hotmail.com>
2024-09-08 10:56:23 -0300

    Configuração do EntityFramework

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace mf_api_web_services_fuel_manager.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+                
+        }
+
+        public DbSet<Veiculo> Veiculos { get; set; }
+
+        public DbSet<Consumo> Consumos { get; set; }   
+    }
+}
